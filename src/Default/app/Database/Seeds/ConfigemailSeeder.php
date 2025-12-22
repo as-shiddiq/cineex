ac1476cadaa1371015f12373325b1accde28d710
Nasrullah Siddik <n.shiddiq@gmail.com>
2025-12-22 18:49:43 +0800

    Update ConfigemailSeeder with actual email configuration

--- src/Default/app/Database/Seeds/ConfigemailSeeder.php
+++ src/Default/app/Database/Seeds/ConfigemailSeeder.php
@@ -8,22 +8,28 @@ class ConfigemailSeeder extends Seeder
 {
     public function run()
     {
-        $data = [
-            'id' => uuid(),
-            'config_email_nama' => 'Cineex',
-            'config_email_host'=>'mail.kodingakan.com',
-            'config_email_smptsecure'=>'tls',
-            'config_email_smtpauth'=>'TRUE',
-            'config_email_username'=>'noreply@kodingakan.com',
-            'config_email_password'=>'------',
-            'config_email_port'=>'587',
-            'config_email_footnote'=>'<p>Cineex (CodeIgniter Next and Extendable)</p>',
-            'config_email_footer'=>'<p>Powered by <a href="https://kodingakan.com">Cineex.</a></p>',
-
-
+        helper('app');
+        $Model = new \App\Models\ConfigemailModel();
+        $ar = [   
+            [   
+                'id' => uuid(),
+                'config_email_nama' => 'Cineex',
+                'config_email_host'=>'mail.kodingakan.com',
+                'config_email_smptsecure'=>'tls',
+                'config_email_smtpauth'=>'TRUE',
+                'config_email_username'=>'noreply@kodingakan.com',
+                'config_email_password'=>'------',
+                'config_email_port'=>'587',
+                'config_email_footnote'=>'<p>Cineex (CodeIgniter Next and Extendable)</p>',
+                'config_email_footer'=>'<p>Powered by <a href="https://kodingakan.com">Cineex.</a></p>',
+            ]
         ];
-
-        // Insert data
-        $this->db->table('configemail')->insert($data);
+       foreach ($ar as $k=>$v) {
+            $check = $Model->first();
+            if($check == null)
+            {
+                $Model->insert($v);
+            }
+        }
     }
 }
